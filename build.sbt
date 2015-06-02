6baedb76d686185116e6e773a90cd03ba41265d0
Lee Mighdoll <lee@nestlabs.com>
2015-06-02 14:54:04 -0700

    simple-fied example of the problem: see comments in build.sbt . within sbt, run deps netty-3 to test with different sbt versions (or sbt settings)   . if resolution is done correctly, a netty-3 jar should be on the classpath

--- build.sbt
+++ build.sbt
@@ -1,14 +1,26 @@
-val sparkCore             = "org.apache.spark"         %% "spark-core"                            % "1.2.2"
-val sparkMllib            = "org.apache.spark"         %% "spark-mllib"                           % "1.2.2"
-val sparkCassandra        = "com.datastax.spark"       %% "spark-cassandra-connector"             % "1.2.1"
+val vSpark = "1.3.1"
+val sparkCore             = "org.apache.spark"         %% "spark-core"                % vSpark
+val sparkCassandra        = "com.datastax.spark"       %% "spark-cassandra-connector" % "1.3.0-M1"
+val cassandraDriver       = "com.datastax.cassandra"    %  "cassandra-driver-core"     % "2.1.6"
+
+import sbt.complete.DefaultParsers._
 
 lazy val root = (project in file(".")).
   settings(
     scalaVersion := "2.11.6",
+    updateOptions := updateOptions.value.withCachedResolution(true),  // this breaks it
+    dependencyOverrides += cassandraDriver,   // this fixes it!
     libraryDependencies ++= Seq(
       sparkCore,
-      sparkCassandra,
-      sparkMllib
-    ) 
+      cassandraDriver,
+      sparkCassandra
+    ),
+    deps := {
+      val matchString = (Space ~> token(StringBasic,"<search string>")).parsed
+      val classpath = (fullClasspath in Compile).value
+      val found = classpath.map(_.data.toString).filter(_.contains(matchString))
+      found foreach println
+    }
   )
 
+lazy val deps = inputKey[Unit]("search classpath")
---
+++ project/Dependencies.scala
@@ -0,0 +1,246 @@
+import sbt._
+
+object Dependencies {
+  object V {
+    val scalaTest = "2.2.1"
+    val scalaCheck = "1.12.1" 
+    val akka = "2.3.11"
+    val spray = "1.3.3"
+    val sprayJson = "1.3.2"
+    val spark = "1.2.2"
+    val slf4j = "1.7.9"
+  }
+
+  // To get latest versions
+  val slf4j                 = "org.slf4j"                 % "slf4j-api"               % V.slf4j
+  val scalaConfig           = "com.typesafe"              %  "config"                 % "1.2.1"
+
+  // Spray + Akka
+  val sprayCan              = "io.spray"                  %%  "spray-can"              % V.spray
+  val sprayRouting          = "io.spray"                  %%  "spray-routing"          % V.spray
+  val sprayClient           = "io.spray"                  %%  "spray-client"           % V.spray
+  val sprayCaching          = "io.spray"                  %%  "spray-caching"          % V.spray
+  val sprayUtil             = "io.spray"                  %%  "spray-util"             % V.spray
+  val sprayJson             = "io.spray"                  %%  "spray-json"             % V.sprayJson
+  val akkaActor             = "com.typesafe.akka"         %%  "akka-actor"             % V.akka
+  val akkaSlf4j             = "com.typesafe.akka"         %%  "akka-slf4j"             % V.akka
+  val akkaRemoting          = "com.typesafe.akka"         %%  "akka-remote"            % V.akka
+
+  val argot                 = "org.clapper"               %% "argot"                  % "1.0.3"
+  val nScalaTime            = "com.github.nscala-time"    %% "nscala-time"            % "1.6.0"
+  val spire                 = "org.spire-math"            %% "spire"                  % "0.9.0"
+  val openCsv               = "net.sf.opencsv"            %  "opencsv"                % "2.3"
+  val cassandraAll          = "org.apache.cassandra"      %  "cassandra-all"          % "2.1.2"
+  val cassandraDriver       = "com.datastax.cassandra"    %  "cassandra-driver-core"  % "2.1.6"
+  val snappy                = "org.xerial.snappy"         %  "snappy-java"            % "1.0.5"
+  val lz4                   = "net.jpountz.lz4"           %  "lz4"                    % "1.3.0"
+
+  // Note: version is intentionally left out. Usage: `libraryDependencies <+= scalaVersion(scalaReflect % _)`.
+  val scalaReflect          = "org.scala-lang"            %  "scala-reflect"
+  val scalaLogging          = "com.typesafe.scala-logging" %% "scala-logging-slf4j"    % "2.1.2"
+
+  val rxScala               = "io.reactivex"              %% "rxscala"                % "0.23.1"
+  val sparkCassandra        = ("com.datastax.spark"       %% "spark-cassandra-connector" % "1.2.1" withSources() withJavadoc()
+                                excludeAll(ExclusionRule(organization = "org.apache.spark"))
+                              )
+  val sparkCore             = ("org.apache.spark"         %% "spark-core"             % V.spark
+                                // a shaded version of minlog included in the kryo jar, so try to exclude this additional copy
+                                // however, this doesn't seem to work, so we yank it out of the assembly
+                                // LATER revisit in a current rev of spark/kryo. See https://github.com/EsotericSoftware/kryo/issues/189
+                                exclude("com.esotericsoftware.minlog", "minlog")
+                                exclude("org.slf4j", "slf4j-log4j12")
+                              )
+  val sparkSql              = ("org.apache.spark"         %% "spark-sql"             % V.spark
+                                exclude("org.slf4j", "slf4j-log4j12")
+                              )
+  val sparkMllib            = ("org.apache.spark"         %% "spark-mllib"            % V.spark
+                                exclude("com.typesafe", "scalalogging-slf4j_2.11")   // avoid version conflict (and selected explicitly above anyway)
+                                exclude("org.slf4j", "slf4j-log4j12")
+                              )
+  val sparkRepl             = ("org.apache.spark"          %% "spark-repl"             % V.spark    
+                                exclude("org.slf4j", "slf4j-log4j12")
+                              )
+
+  val breeze                = "org.scalanlp"               %% "breeze"                 % "0.9"
+  // we can add this back to speed up breeze
+  val breezeNatives         = ("org.scalanlp"              %% "breeze-natives"         % "0.9"
+                                exclude("com.github.fommil.netlib", "all")
+                              )
+  val nativeNetlibLinux     = "com.github.fommil.netlib" % "netlib-native_system-linux-x86_64" % "1.1" classifier "natives"
+  val nativeNetlibOsX       = "com.github.fommil.netlib" % "netlib-native_system-osx-x86_64" % "1.1" classifier "natives"
+                              
+  val apacheAvro            = "org.apache.avro"           % "avro"                    % "1.7.6"
+  // avroMapred comes in a hadoop1 vs. hadoop2 favor
+  // All spark 1.1.0 artifacts are compiled against hadoop1 libraries, so we want to the "hadoop1" classifier here
+  val apacheAvroMapred      = ("org.apache.avro"          %  "avro-mapred"            % "1.7.6" classifier "hadoop1"
+                                exclude("org.slf4j", "slf4j-api")
+                                exclude("org.apache.avro", "avro")
+                                exclude("org.apache.avro", "avro-ipc")
+                                exclude("org.codehaus.jackson", "jackson-core-asl")
+                                exclude("org.codehaus.jackson", "jackson-mapper-asl")
+                              )
+
+  val apacheKafka           = ("org.apache.kafka"         %% "kafka"                  % "0.8.2.1"
+                                  exclude("javax.jms", "jms")
+                                  exclude("com.sun.jdmk", "jmxtools")
+                                  exclude("com.sun.jmx", "jmxri")
+                                  exclude("org.slf4j", "slf4j-simple")
+                              )
+  val zookeeper             = ("org.apache.zookeeper"      % "zookeeper"              % "3.4.5"
+                                  exclude ("org.jboss.netty", "netty")
+                                  exclude("javax.jms", "jms")
+                                  exclude("com.sun.jdmk", "jmxtools")
+                                  exclude("com.sun.jmx", "jmxri")
+                              )
+
+  val unfiltered            = "net.databinder"            %% "unfiltered-netty-websockets"  % "0.8.4"
+  val sparkNetty            = "io.netty"                  %  "netty"                  % "3.8.0.Final"
+
+  val metricsScala          = "nl.grons"                  %% "metrics-scala"          % "3.5.1"
+  val metricsGraphite       = "io.dropwizard.metrics"     %  "metrics-graphite"       % "3.1.2"
+
+  // match version used by spark, cassandra driver, etc
+  val guava                 = "com.google.guava"          % "guava"                   % "14.0.1"
+
+  // see https://issues.scala-lang.org/browse/SI-8978
+  val jsr                   = "com.google.code.findbugs"  % "jsr305"                  % "2.0.3"
+
+  object Runtime {
+    val logback             = "ch.qos.logback"            % "logback-classic"         % "1.1.2"
+    val log4j               = "log4j"                     % "log4j"                   % "1.2.17"
+    val slf4jlog4j          = "org.slf4j"                 % "slf4j-log4j12"           % V.slf4j
+    val log4jBridge         = "org.slf4j"                 % "log4j-over-slf4j"        % V.slf4j
+  }
+
+  object Test {
+    val scalaTest           = "org.scalatest"            %% "scalatest"              % V.scalaTest  % "test"
+    val scalaCheck          = "org.scalacheck"           %% "scalacheck"             % V.scalaCheck % "test"
+    val sprayTestKit        = "io.spray"                 %% "spray-testkit"          % V.spray      % "test"
+    val akkaTestKit         = "com.typesafe.akka"        %% "akka-testkit"           % V.akka       % "test"
+  }
+
+  object IT {
+    val scalaTest           = "org.scalatest"            %% "scalatest"              % V.scalaTest  % "it"
+    val scalaCheck          = "org.scalacheck"           %% "scalacheck"             % V.scalaCheck % "it"
+    val sprayTestKit        = "io.spray"                 %% "spray-testkit"          % V.spray      % "it"
+    val akkaTestKit         = "com.typesafe.akka"        %% "akka-testkit"           % V.akka       % "it"
+    val tubeSocks           = ("me.lessis"               %% "tubesocks"              % "0.1.0"
+                                exclude("org.slf4j", "slf4j-jdk14")
+                              )
+  }
+  
+  object Kit {
+    val scalaTest           = "org.scalatest"            %% "scalatest"              % V.scalaTest
+    val scalaCheck          = "org.scalacheck"           %% "scalacheck"             % V.scalaCheck
+    val sprayTestKit        = "io.spray"                 %% "spray-testkit"          % V.spray
+    val akkaTestKit         = "com.typesafe.akka"        %% "akka-testkit"           % V.akka
+  }
+  
+  object Optional {
+    val logback             = Runtime.logback               % "optional"
+    val log4j               = Runtime.log4j                 % "optional"
+    val slf4jlog4j          = Runtime.slf4jlog4j            % "optional"
+    val log4jBridge         = Runtime.log4jBridge           % "optional"
+    val metricsGraphite     = Dependencies.metricsGraphite  % "optional"
+  }
+  
+
+  val basicTest = Seq(
+    Test.scalaTest,
+    Test.scalaCheck,
+    IT.scalaTest,
+    IT.scalaCheck
+  )
+  
+  val allTest = Seq(
+    Test.scalaTest,
+    Test.scalaCheck,
+    Test.sprayTestKit,
+    Test.akkaTestKit,
+    IT.scalaTest,
+    IT.scalaCheck,
+    IT.sprayTestKit,
+    IT.akkaTestKit
+  )
+  
+  // For setting minimum versions
+  val dependencyOverrides = Set(
+      slf4j,
+      Runtime.log4j
+  )
+
+  val logging = Seq(
+    scalaLogging,
+    slf4j
+  )
+
+  val testAndLogging = basicTest ++ logging
+
+  val logbackTest = testAndLogging ++ Seq(
+    Runtime.logback % "test;it"
+  )
+
+  val log4jLogging = Seq(
+    Runtime.slf4jlog4j, 
+    Runtime.log4j
+  )
+  
+  val storeKitTestsAndLogging = {
+    Seq( // test libraries, but in main config, not in Test or IT
+      Kit.scalaTest,
+      Kit.scalaCheck,
+      Kit.akkaTestKit
+    ) ++ logging
+  }
+
+  val kitTestsAndLogging = {
+    Seq( // test libraries, but in main config, not in Test or IT
+      Kit.scalaTest,
+      Kit.scalaCheck,
+      Kit.sprayTestKit,
+      Kit.akkaTestKit
+    ) ++ logging
+  }
+
+  val spray = Seq(
+    sprayJson,
+    sprayClient,
+    sprayRouting,
+    sprayCan,
+    sprayCaching,
+    Test.sprayTestKit,
+    Test.akkaTestKit, // delete this after spray #446 is resolved
+    IT.sprayTestKit,
+    IT.akkaTestKit // delete this after spray #446 is resolved
+  )
+
+  val kafka = Seq(apacheKafka)
+
+  val avro = Seq(apacheAvro)
+
+  val akka = Seq(
+    akkaActor,
+    akkaRemoting,
+    akkaSlf4j
+  )
+
+  val cassandraClient = Seq(
+    cassandraDriver,
+    snappy,
+    lz4
+  )
+
+  val nativeMath = Seq(
+    breeze
+  )
+
+  val spark = Seq(
+    sparkCassandra,
+    sparkMllib,
+    sparkCore,
+    sparkSql,
+    sparkNetty
+  ) ++ nativeMath
+ 
+
+}
